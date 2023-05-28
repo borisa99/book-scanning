c7a4cc7d7ee95f11897a21550879a84602afe692
Borisa Krstic <borisakrstic02@gmail.com>
2023-05-28 21:00:06 +0200

    inital book table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import { type NextPage } from "next";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
+import { api } from "@/utils/api";
+
 const Home: NextPage = () => {
   const user = useUser();
 
+  const { data } = api.books.getAll.useQuery();
+
   return (
     <div className="flex min-h-screen flex-1 items-center justify-center">
-      {!user && <SignInButton />}
-      {!!user && <SignOutButton />}
+      <div className="mr-5">
+        {!user && <SignInButton />}
+        {!!user && <SignOutButton />}
+      </div>
+      <div className="flex flex-col">
+        <h1>Books</h1>
+        {data?.map((book) => (
+          <div key={book.id}>{book.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
