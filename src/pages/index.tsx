96c5e5cbcbc2f3ebfec116449f24522e38b5c5f6
Borisa Krstic <borisakrstic02@gmail.com>
2023-05-28 21:54:09 +0200

    Create book endpoint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,33 @@ import { type NextPage } from "next";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
 import { api } from "@/utils/api";
+import { useState } from "react";
+
+const CreateBookForm = () => {
+  const [title, setTitle] = useState("");
+
+  const { mutate } = api.books.create.useMutation();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate({ title });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="border "
+        type="text"
+        name="title"
+        id="title"
+        placeholder="Book title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+    </form>
+  );
+};
 
 const Home: NextPage = () => {
   const user = useUser();
@@ -9,11 +36,12 @@ const Home: NextPage = () => {
   const { data } = api.books.getAll.useQuery();
 
   return (
-    <div className="flex min-h-screen flex-1 items-center justify-center">
+    <div className="flex min-h-screen flex-1 flex-col items-center justify-center gap-10">
       <div className="mr-5">
         {!user && <SignInButton />}
         {!!user && <SignOutButton />}
       </div>
+      <CreateBookForm />
       <div className="flex flex-col">
         <h1>Books</h1>
         {data?.map((book) => (
--- src/server/api/routers/books.ts
+++ src/server/api/routers/books.ts
@@ -1,8 +1,24 @@
-// import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { z } from "zod";
+import { createTRPCRouter, privateProcedure } from "@/server/api/trpc";
 
 export const booksRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.book.findMany();
+  getAll: privateProcedure.query(({ ctx }) => {
+    return ctx.prisma.book.findMany({ orderBy: { createdAt: "desc" } });
   }),
+  create: privateProcedure
+    .input(
+      z.object({
+        title: z.string(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const book = await ctx.prisma.book.create({
+        data: {
+          authorId: ctx.userId,
+          title: input.title,
+        },
+      });
+
+      return book;
+    }),
 });
